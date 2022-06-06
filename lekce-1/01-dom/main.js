de9e3eeac1eaadd6ce75ef52428608a12f6825f2
blatnak <katerina.blatna@uzis.cz>
2022-06-06 15:16:41 +0200

    first lesson with home exercises

---
+++ lekce-1/01-dom/main.js
@@ -0,0 +1,9 @@
+// tady je místo pro náš program
+
+document.querySelector('.main-title').style.color = 'lightblue'
+
+document.querySelector('.text-paragraph').style.fontSize = '20px'
+
+document.querySelector('.animal-image').src = 'pes.jpg'
+
+document.querySelector('.ctverecek').style.transform = 'rotate(30deg)'
